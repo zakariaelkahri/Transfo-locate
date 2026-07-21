1c872e69dcd466ba6a90beac6dc47550dbc02082
zakariaelkahri <zakariakahri2003@gmail.com>
2026-07-21 11:56:38 +0100

    fixig docker compose for prod

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,25 +1,27 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.api import health, stock
-from contextlib import asynccontextmanager
 from app.core.database import init_db
 from app.core.config import settings
-from app.seeders.stock_seeder import seed_stock
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    print("Initializing database...")
-    init_db()
-    seed_stock()
-    print("Database initialized successfully!")
 
-    yield
 
-    print("Shutting down application...")
+# from contextlib import asynccontextmanager
+# from app.seeders.stock_seeder import seed_stock
+# @asynccontextmanager
+# async def lifespan(app: FastAPI):
+#     print("Initializing database...")
+#     init_db()
+#     seed_stock()
+#     print("Database initialized successfully!")
+
+#     yield
+
+#     print("Shutting down application...")
 
 
 app = FastAPI(
     title=settings.app_name,
-    lifespan=lifespan,
+    # lifespan=lifespan,
 )
 
 app.add_middleware(
