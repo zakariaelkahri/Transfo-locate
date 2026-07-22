c7435adc6f683225401c687d522dfa9cafe58b3a
zakariaelkahri <zakariakahri2003@gmail.com>
2026-07-22 09:57:54 +0100

    final docker compose v1

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -3,15 +3,12 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     app_name: str = "stock location"
-
-
-    SAGE_API_URL: str 
+    SAGE_API_URL: str
     SAGE_API_USER: str
     SAGE_API_PASSWORD: str
 
     class Config:
         env_file = ".env"
         case_sensitive = True
-
 
 settings = Settings()
