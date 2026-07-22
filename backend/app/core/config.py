32e0a73c55a380006c6c3f77e104584386f90a08
zakariaelkahri <zakariakahri2003@gmail.com>
2026-07-22 09:56:21 +0100

    final docker compose v1

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -4,9 +4,9 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     app_name: str = "stock location"
 
-    DATABASE_URL: str
-    SAGE_API_URL: str = "http://192.168.10.100:8124/api1/x3/erp/ENERGY/STOCK"
-    SAGE_API_USER: str = "API"
+
+    SAGE_API_URL: str 
+    SAGE_API_USER: str
     SAGE_API_PASSWORD: str
 
     class Config:
