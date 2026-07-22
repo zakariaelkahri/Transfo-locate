cdc2fb636c3a236572371c6708f53df7a88be89c
zakariaelkahri <zakariakahri2003@gmail.com>
2026-07-22 16:40:32 +0100

    add search by lot

--- backend/app/api/stock.py
+++ backend/app/api/stock.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from app.schemas.stock import ItemRequest, LotRequest, SloRequest
+from app.schemas.stock import ItemRequest, LotRequest
 from app.services.item_loc import get_item_loc
 from app.core.config import settings
 import requests
@@ -36,7 +36,7 @@ def item_loc(item_ref: ItemRequest):
 
 
 @router.post("/item_loc/lot")
-def item_loc(lot: LotRequest):
+def item_loc_by_lot(lot: LotRequest):
     response = requests.get(
         f"{settings.SAGE_API_URL}"
         f"?representation=STOCK.$lookup&where=LOT%20eq%20%27{lot.lot}%27",
@@ -47,7 +47,7 @@ def item_loc(lot: LotRequest):
 
     items = []
     for res in final_response["$resources"]:
-        if res["LOCTYP"] == "ZONE1" :
+        if res["LOCTYP"] == "ZONE1":
             loc = get_item_loc(res["LOC"])
             item = {
                 "LOT": res["LOT"],
@@ -62,30 +62,3 @@ def item_loc(lot: LotRequest):
 
     return items
 
-
-# @router.post("/item_loc/slo")
-# def item_loc_by_slo(slo: SloRequest):
-#     response = requests.get(
-#         f"{settings.SAGE_API_URL}"
-#         f"?representation=STOCK.$lookup&where=SLO%20eq%20%27{slo.slo}%27",
-#         auth=(settings.SAGE_API_USER, settings.SAGE_API_PASSWORD)
-#     )
-
-#     final_response = response.json()
-
-#     items = []
-#     for res in final_response["$resources"]:
-#         if res["LOCTYP"] == "ZONE1":
-#             loc = get_item_loc(res["LOC"])
-#             item = {
-#                 "LOT": res["LOT"],
-#                 "SLO": res["SLO"],
-#                 "LOCTYP": res["LOCTYP"],
-#                 "QTYSTU": res["QTYSTU"],
-#                 "STOFCY": res["STOFCY"],
-#                 "ITMREF": res["ITMREF"],
-#                 "LOC": loc,
-#             }
-#             items.append(item)
-
-#     return items
