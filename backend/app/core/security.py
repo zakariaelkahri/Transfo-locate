6f71cc9dda5de9ac68987c8eb3cbc69b1274c5a9
zakariaelkahri <zakariakahri2003@gmail.com>
2026-07-29 16:53:53 +0100

    auth for admin and users added

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -8,6 +8,7 @@ from app.core.config import settings
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
+
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
 
