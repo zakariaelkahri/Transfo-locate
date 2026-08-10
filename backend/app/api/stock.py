2a279769faade43f94072d845f604f0427c369ce
zakariaelkahri <zakariakahri2003@gmail.com>
2026-08-10 13:01:55 +0100

    the first v of materials aloccated done !

--- backend/app/api/stock.py
+++ backend/app/api/stock.py
@@ -100,9 +100,9 @@ def item_loc_by_serie_num(serie_num: SerieNumRequest):
 
     return items, {"pagination_existance": out_of_range_items}
 
+
 @router.post("/matloc/OF")
 def get_of_mat(OF: OFRequest):
-
     response_mat = requests.get(
         f"{settings.SAGE_API_URL_MAT}"
         f"?representation=MFGMAT.$lookup&count=1000&where=MFGNUM%20eq%20%27{OF.OF}%27",
@@ -111,29 +111,31 @@ def get_of_mat(OF: OFRequest):
     mat_items = []
     resources_mfgmat = response_mat.json()
 
-    for i in resources_mfgmat["$resources"] :
-        if i["ALLQTY"] > 0 :
-            print(i["ALLQTY"])
+    for i in resources_mfgmat["$resources"]:
+        if i["ALLQTY"] > 0:
+            mfglin = i["MFGLIN"]
+            itmref = i["ITMREF"]
             response_stoall = requests.get(
-            f"{settings.SAGE_API_URL_STOALL}"
-            f"?representation=STOALL.$lookup&count=1000&where=VCRNUM%20eq%20%27{OF.OF}%27 and VCRLIN eq {i["MFGLIN"]} and VCRTYP eq 10 and ITMREF eq %27{i["ITMREF"]}%27",
-            auth=(settings.SAGE_API_USER, settings.SAGE_API_PASSWORD)
+                f"{settings.SAGE_API_URL_STOALL}"
+                f"?representation=STOALL.$lookup&count=1000&where=VCRNUM%20eq%20%27{OF.OF}%27"
+                f" and VCRLIN eq {mfglin} and VCRTYP eq 10 and ITMREF eq %27{itmref}%27",
+                auth=(settings.SAGE_API_USER, settings.SAGE_API_PASSWORD)
             )
 
             resources_stoall = response_stoall.json()
 
-            for j in resources_stoall["$resources"] :
+            for j in resources_stoall["$resources"]:
                 stock_item = {}
+                stocou = j["STOCOU"]
                 response_stock = requests.get(
-                f"{settings.SAGE_API_URL_STOCK}"
-                f"?representation=STOCK.$lookup&count=1000&where=STOFCY%20eq%20%27SIEGE%27 and STOCOU eq {j["STOCOU"]}",
-                auth=(settings.SAGE_API_USER, settings.SAGE_API_PASSWORD)
+                    f"{settings.SAGE_API_URL_STOCK}"
+                    f"?representation=STOCK.$lookup&count=1000&where=STOFCY%20eq%20%27SIEGE%27 and STOCOU eq {stocou}",
+                    auth=(settings.SAGE_API_USER, settings.SAGE_API_PASSWORD)
                 )
 
-                if j["STOCOU"] > 0 :
+                if j["STOCOU"] > 0:
                     stock = response_stock.json()
                     res = stock["$resources"]
-                    print(res[0]["LOCTYP"])
                     if res[0]["LOCTYP"] == "ZONE1":
                         loc = get_item_loc(res[0]["LOC"])
                         stock_item = {
@@ -146,19 +148,17 @@ def get_of_mat(OF: OFRequest):
                             "SERNUM": res[0]["SERNUM"],
                             "STA": res[0]["STA"],
                             "LOC": loc,
-
                         }
                     else:
                         continue
 
-                item_final= {
-                "No ordre": i["MFGNUM"],
-                "No ligne": i["MFGLIN"],
-                "Article": i["ITMREF"],
-                "Quantité allouée": i["ALLQTY"],
-                "stock infos": stock_item,
+                item_final = {
+                    "No ordre": i["MFGNUM"],
+                    "No ligne": i["MFGLIN"],
+                    "Article": i["ITMREF"],
+                    "Quantité allouée": i["ALLQTY"],
+                    "stock infos": stock_item,
                 }
                 mat_items.append(item_final)
-
 
     return mat_items
